62f88b13ea0e9ef6ba10b393a860a48ceafa40cb
ZEN1X <20771421+ZEN1X@users.noreply.github.com>
2024-12-11 01:05:27 +0100

    Create navier_stokes source and header files, define constants

---
+++ 07_JanKwiatkowski/src/navier_stokes.cpp
@@ -0,0 +1,11 @@
+#include "navier_stokes.h"
+
+constexpr double DELTA = 0.01;
+constexpr double RHO = 1.0;
+constexpr double MU = 1.0;
+constexpr int NX = 200;
+constexpr int NY = 90;
+constexpr int I1 = 50;
+constexpr int J1 = 55;
+constexpr int J2 = J1 + 2;
+constexpr int IT_MAX = 20000;
---
+++ 07_JanKwiatkowski/src/navier_stokes.h
@@ -0,0 +1,6 @@
+#ifndef NAVIER_STOKES_H
+#define NAVIER_STOKES_H
+
+
+
+#endif /* NAVIER_STOKES_H */
